57f339334bd37d7e9d8932ca0278120d7d81d4dd
sharkgenus <maxxer802@gmail.com>
2026-02-27 06:10:04 +0100

    Major overhaul to main.py, new modular menu implemented, partial overhaul of input.py and analysis.py, new folder to store output files called outputs. More coming soon

--- analysis.py
+++ analysis.py
@@ -5,40 +5,62 @@ import wave
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-#After this make this input into a variable storing a .wav file so multiple/any .wav files can be saved and used
-#Maybe better to make this a class with a class function doing the below, then can store new .wav files as instances of that class and operate on them thusly
+scrip_dir = Path(__file__).resolve.parent # Finds the parent directory of this currently running script
 
-scr_dir = Path(__file__).resolve().parent
-filepath = scr_dir / "output.wav"
+class audio_input_file:
 
-#Using pythons Wave module to parse the .wav file, the "rb" tag means read binary
+    def __init__(self, fname, ftype):
+        self.fname = fname
+        self.ftype = ftype #should be 3 options ".wav" "mp3" etc
+        self.fpath = script_dir / fname 
+
+    def filename(self)
+        filepath = 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
+class audio_object: # This will be any instance of audio, we will pull it's data and store it as this class
+
+    def __init__(self, n_channels, sample_width, sample_rate, n_frames,):
+        
+        self.n_channels = n_channels
+        self.sample_width = sample_width
+        self.sample_rate = sample_rate
+        self.n_frames = n_frames 
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+#so, this module should act on a class object called audio_input_file, which will have a filetype and a filepath
+#It will pull the ftype and then pull the data accordingly (now only .wav)
+#Then, it will make a new instance of the audio_object class, filling all the requisite variables
+
+
+# Finds path of currently running program, gets it's parent directory
+script_dir = Path(__file__).resolve().parent
+filepath = script_dir / "output.wav"
+
+# Wave module parses the .wav file, "rb" tag -> read binary
 with wave.open(str(filepath), "rb") as wf :
 
-#Important data of the .wav file is stored in variables
-#Channel info (1,2 --> mono, sterio) as n_channels
-#Sample width (1,2,4...(bytes) --> 8 bit, 16 bit, 32 bit ...) as sample_width
-#Samples per second (44100, 48000, 96000 etc) as sample_rate
-#Number of frames as n_frames
-    n_channels = wf.getnchannels()
-    sample_width = wf.getsampwidth()
-    sample_rate = wf.getframerate()
-    n_frames = wf.getnframes()
+    n_channels = wf.getnchannels() # Channel info (1 -> mono, 2 -> sterio)
+    sample_width = wf.getsampwidth() # Sample width (1 -> 8 bit, 2 -> 16 bit, 4 -> 32 bit)
+    sample_rate = wf.getframerate() # Samples per second
+    n_frames = wf.getnframes() # Number of frames
 
-#Then the wf.readframes command reads audio data in the .wav file and returns those bytes in the variable frames 
-    frames = wf.readframes(n_frames)
-
-#The framerate is stored as fmt with the struct format string "<{}B,h, or i " to denote  Little-endian and B,h, or i depending on the sample width (to denote 8, 16, or 32 bit samples)
+# The framerate is stored as fmt with the correct struct format string depending on if it is 8, 16, or 32 bit
 if sample_width == 1:
     fmt = "<{}B".format(n_frames * n_channels)
 elif sample_width == 2:
     fmt = "<{}h".format(n_frames * n_channels)
 elif sample_width == 4:
     fmt = "<{}i".format(n_frames * n_channels)
-#Print's error if sample width is not 8, 16, or 32 bit
+# Print's error if sample width is not 8, 16, or 32 bit
 else:
     raise ValueError("Unsupported sample width: {}".format(sample_width))
 
-#Finally, struct unpacks the data from the fmt and frames variables into a numpy array stored in the variable samples
+# Finally, struct unpacks the data from the fmt and frames variables into a numpy array stored in the variable samples
 samples = np.array(struct.unpack(fmt, frames))
 
 #Split the data if it's stereo in preparation for fourier transform
@@ -49,23 +71,24 @@ else:
     left = samples
     right = None
 
-#Normalize values of sample to between -1 and 1
+# Normalize sample values between -1, 1
 if sample_width == 1:
-    left = (left - 128) / 128  # 8-bit samples are unsigned, so -128 to shift values from 0, 256 to -128, 128 first
+    left = (left - 128) / 128  # 8-bit samples are unsigned --> subtract 128 to shift values from 0, 256 to -128, 128
 elif sample_width == 2:
-    left = left / 32768        # 16-bit samples are between - and + 32768
+    left = left / 32768        # 16-bit samples signed between - and + 32768
 elif sample_width == 4:
     left = left / 2147483648   # 32-bit signed - and + 2147483648
 
+# Create 3 plots
 fig, ax = plt.subplots(1, 3, figsize=(15,5))
 
-# Original signal
+# Plot the original signal
 ax[0].plot(left)
 ax[0].set_title("Signal")
 ax[0].set_xlabel("Sample Index")
 ax[0].set_ylabel("Amplitude")
 
-# FFT
+# Plot the FFT in two parts
 fft = np.fft.fft(left)
 
 # Real part
@@ -80,5 +103,6 @@ ax[2].set_title("Imaginary Part of FFT")
 ax[2].set_xlabel("Frequency Index")
 ax[2].set_ylabel("Magnitude")
 
+# Clean up plotting errors and show
 plt.tight_layout()
 plt.show()
--- input.py
+++ input.py
@@ -2,44 +2,126 @@ import numpy as np
 import wave 
 import pyaudio
 import os
+from datetime import datetime # Here I'm importing the datetime class from within the module datetime
+import time
+import threading
+import keyboard
+
+# Get current date and time
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S") #Save string "year-month-day_hour-minute-second"
+fname = timestamp # Generic filename variable to hold the timestamp string, for when renaming files functionality added
 
 
-print("Do you want to record?")
-mpekas = input()
-while mpekas != "y" :
-     print("change your mind yet?")
-     mpekas = input()
-print("How many second you want to record?")
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
-sample_form = pyaudio.paInt16
-sr = 44100
-channels = 1
-sec = float(input())
-chunk = 1024
-wave_out = os.path.join(os.path.dirname(os.path.abspath(__file__)),"output.wav")
-print("saving to" , wave_out)
 
-p = pyaudio.PyAudio()
+def keystroke_record():
+     sample_form = pyaudio.paInt16
+     sr = 44100
+     channels = 1
+     chunk = 1024
 
-stream = p.open(format=sample_form,
-                channels=channels,
-                rate=sr,
-                frames_per_buffer=chunk,
-                input=True,)
-print("mprokologo twra perimene gt grafw")
-frames = []
-for i in range(0,int(sr/chunk * sec)) :
-     data = stream.read(chunk)
-     frames.append(data)
+     p = pyaudio.PyAudio()
 
-stream.stop_stream()
-stream.close()
+     print("Press SPACE to start recording...")
+     keyboard.wait("space") # Waits for spacebar input
 
-p.terminate()
+     print("Recording... Press SPACE again to stop.")
 
-wf = wave.open(wave_out, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_form))
-wf.setframerate(sr)
-wf.writeframes(b''.join(frames))
-wf.close())
+     stream = p.open(format=sample_form,
+                         channels=channels,
+                         rate=sr,
+                         frames_per_buffer=chunk,
+                         input=True,)
+          
+     frames = []
+     recording = True
+
+     while recording:
+          data = stream.read(chunk)
+          frames.append(data)
+
+          if keyboard.is_pressed("space"):
+               recording = False
+
+     print("Recording stopped.")
+
+     stream.stop_stream()
+     stream.close()
+
+     p.terminate()
+     
+     wave_out = os.path.join(os.path.dirname(os.path.abspath(__file__)), "outputs", f"{fname}.wav")
+
+     wf = wave.open(wave_out, 'wb')
+     wf.setnchannels(channels)
+     wf.setsampwidth(p.get_sample_size(sample_form))
+     wf.setframerate(sr)
+     wf.writeframes(b''.join(frames))
+     wf.close()
+
+     print("Saved to" , wave_out) # Print location where file was saved
+
+     
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
+def fixed_interval_record():
+     while True:
+          try: # Checks if duration is acceptable, if so break, if not print error
+               duration = int(input("How many seconds do you want to record? "))
+               if 0 < duration < 61: # Could make a variable here so max time can be changed, a bit overkill tho
+                    break
+          except:
+               print("Invalid input. Please enter an integer between 1 and 60")
+
+     def counter(duration):
+          for i in range(1, duration + 1):
+               print(f"Recording... {i}", end='\r')  # '\r' overwrites the line
+               time.sleep(1)  # wait 1 second
+
+          print("\nRecording finished!")
+
+     def record(duration):
+          sample_form = pyaudio.paInt16
+          sr = 44100
+          channels = 1
+          chunk = 1024
+
+          #Stores concatenated string of parent directory + filename + .wav 
+          wave_out = os.path.join(os.path.dirname(os.path.abspath(__file__)), "outputs", f"{fname}.wav")
+
+          p = pyaudio.PyAudio() 
+
+          stream = p.open(format=sample_form,
+                         channels=channels,
+                         rate=sr,
+                         frames_per_buffer=chunk,
+                         input=True,)
+          
+          frames = []
+          for i in range(0,int(sr/chunk * duration)) :
+               data = stream.read(chunk)
+               frames.append(data)
+
+          stream.stop_stream()
+          stream.close()
+
+          p.terminate()
+
+          wf = wave.open(wave_out, 'wb')
+          wf.setnchannels(channels)
+          wf.setsampwidth(p.get_sample_size(sample_form))
+          wf.setframerate(sr)
+          wf.writeframes(b''.join(frames))
+          wf.close()
+
+          print("Saved to" , wave_out) # Print location where file was saved
+
+     counter_thread = threading.Thread(target=counter, args=(duration,))
+
+     # Both counter and record run simultaneously using threading
+     counter_thread.start()
+     record(duration)
+
